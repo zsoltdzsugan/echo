33c711353e204338787c2c8727c8495e2700cf10
zsolt.dzsugan <zsolt.dzsugan@gmail.com>
2025-04-10 16:05:27 +0200

    feat: show created game view

---
+++ resources/js/pages/games/show.tsx
@@ -0,0 +1,25 @@
+import { PlaceholderPattern } from '@/components/ui/placeholder-pattern';
+import AppLayout from '@/layouts/app-layout';
+import { type BreadcrumbItem } from '@/types';
+import { Head } from '@inertiajs/react';
+
+const breadcrumbs: BreadcrumbItem[] = [
+    {
+        title: 'Game',
+        href: '/game/show',
+    },
+];
+
+export default function Dashboard() {
+    return (
+        <AppLayout breadcrumbs={breadcrumbs}>
+            <Head title="Game" />
+            <div className="flex h-full flex-1 flex-col gap-4 rounded-xl p-4">
+                <div className="border-sidebar-border/70 dark:border-sidebar-border relative min-h-[100vh] flex-1 overflow-hidden rounded-xl border md:min-h-min">
+                    <PlaceholderPattern className="absolute inset-0 size-full stroke-neutral-900/20 dark:stroke-neutral-100/20" />
+                    test
+                </div>
+            </div>
+        </AppLayout>
+    );
+}
