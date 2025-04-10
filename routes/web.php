3625542b5397d3892f46d7a4ee8f7df1fce2e0b3
zsolt.dzsugan <zsolt.dzsugan@gmail.com>
2025-04-10 16:04:12 +0200

    feat: Game resource route

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\GameController;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
@@ -11,6 +12,10 @@ Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('dashboard', function () {
         return Inertia::render('dashboard');
     })->name('dashboard');
+});
+
+Route::middleware('auth')->group(function () {
+    Route::resource('games', GameController::class)->only(['store', 'show']);
 });
 
 require __DIR__.'/settings.php';
