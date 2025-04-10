1559272b4748054d96c0bfacb65ef12758fa4f10
zsolt.dzsugan <zsolt.dzsugan@gmail.com>
2025-04-10 17:07:53 +0200

    feat: logo added

--- resources/js/pages/dashboard.tsx
+++ resources/js/pages/dashboard.tsx
@@ -1,9 +1,11 @@
 import { Button } from '@/components/ui/button';
+import { Input } from '@/components/ui/input';
+import { Label } from '@/components/ui/label';
 import { PlaceholderPattern } from '@/components/ui/placeholder-pattern';
 import AppLayout from '@/layouts/app-layout';
 import { type BreadcrumbItem } from '@/types';
 import { Head, Link } from '@inertiajs/react';
-import { Gamepad2 } from 'lucide-react';
+import { Gamepad2, LogIn } from 'lucide-react';
 
 const breadcrumbs: BreadcrumbItem[] = [
     {
@@ -18,20 +20,30 @@ export default function Dashboard() {
             <Head title="Dashboard" />
             <div className="flex h-full flex-1 flex-col gap-4 rounded-xl p-4">
                 <div className="grid auto-rows-min gap-4 md:grid-cols-3">
-                    <div className="border-sidebar-border/70 dark:border-sidebar-border relative aspect-video overflow-hidden rounded-xl border">
+                    <div className="border-sidebar-border/70 dark:border-sidebar-border relative aspect-square overflow-hidden rounded-xl border">
                         <PlaceholderPattern className="absolute inset-0 size-full stroke-neutral-900/20 dark:stroke-neutral-100/20" />
                     </div>
-                    <div className="border-sidebar-border/70 dark:border-sidebar-border relative aspect-video overflow-hidden rounded-xl border">
-                        <div className="flex items-center justify-center w-full h-full p-2">
+                    <div className="relative flex flex-col gap-4 h-full w-full max-w-xs mx-auto aspect-square overflow-hidden rounded-xl">
+                            <img src="/assets/logo_outline.png" className="h-auto max-w-[320px] mx-auto sm:w-32 md:w-44 lg:w-full"/>
                             <Button asChild size={'lg'} variant={'default'} className='cursor-pointer'>
                                 <Link href={route('games.store')} method="post" as="button">
                                     <Gamepad2 />
                                     Create Game
                                 </Link>
                             </Button>
-                        </div>
+
+                            <div className="mt-4">
+                                <Label>Room Name</Label>
+                                <Input />
+                            </div>
+                            <Button asChild size={'lg'} variant={'secondary'} className='cursor-pointer'>
+                                <Link href={route('games.store')} method="post" as="button">
+                                    <LogIn />
+                                    Join Game
+                                </Link>
+                            </Button>
                     </div>
-                    <div className="border-sidebar-border/70 dark:border-sidebar-border relative aspect-video overflow-hidden rounded-xl border">
+                    <div className="border-sidebar-border/70 dark:border-sidebar-border relative aspect-square overflow-hidden rounded-xl border">
                         <PlaceholderPattern className="absolute inset-0 size-full stroke-neutral-900/20 dark:stroke-neutral-100/20" />
                     </div>
                 </div>
