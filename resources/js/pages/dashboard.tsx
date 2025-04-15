ee148201202615f74dfe0d026893c0242ac22886
zsolt.dzsugan <zsolt.dzsugan@gmail.com>
2025-04-15 12:24:21 +0200

    feat: restructure welcome & dashboard

--- resources/js/pages/dashboard.tsx
+++ resources/js/pages/dashboard.tsx
@@ -1,7 +1,9 @@
+import { Button } from '@/components/ui/button';
+import { Input } from '@/components/ui/input';
 import { PlaceholderPattern } from '@/components/ui/placeholder-pattern';
 import AppLayout from '@/layouts/app-layout';
 import { type BreadcrumbItem } from '@/types';
-import { Head } from '@inertiajs/react';
+import { Head, Link } from '@inertiajs/react';
 
 const breadcrumbs: BreadcrumbItem[] = [
     {
@@ -20,7 +22,35 @@ export default function Dashboard() {
                         <PlaceholderPattern className="absolute inset-0 size-full stroke-neutral-900/20 dark:stroke-neutral-100/20" />
                     </div>
                     <div className="border-sidebar-border/70 dark:border-sidebar-border relative aspect-video overflow-hidden rounded-xl border">
-                        <PlaceholderPattern className="absolute inset-0 size-full stroke-neutral-900/20 dark:stroke-neutral-100/20" />
+                    <div className="relative aspect-video overflow-hidden rounded-xl">
+                        <div className="size-full flex flex-col justify-between gap-4">
+                            <img src="/assets/logo_noheight.png" className="max-w-xs w-full mx-auto h-auto"/>
+                            <div className="flex flex-col h-full justify-center gap-4 max-w-md w-full mx-auto">
+                                <div>
+                                <Button asChild size={'sm'}>
+                                    <Link
+                                        href={route('lobbies.store')}
+                                        method="post"
+                                        className="w-full"
+                                    >
+                                        Create Game
+                                    </Link>
+                                </Button>
+                                </div>
+                                <div>
+                                    <Input placeholder="Lobby ID" />
+                                    <Button asChild size={'sm'}>
+                                        <Link
+                                            href={route('lobbies.store')}
+                                            className="w-full mt-4"
+                                        >
+                                            Join Game
+                                        </Link>
+                                    </Button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                     </div>
                     <div className="border-sidebar-border/70 dark:border-sidebar-border relative aspect-video overflow-hidden rounded-xl border">
                         <PlaceholderPattern className="absolute inset-0 size-full stroke-neutral-900/20 dark:stroke-neutral-100/20" />
